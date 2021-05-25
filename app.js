1fca80531309f0f900d23e844d08b166cea1f986
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-25 19:37:35 -0300

    Sample data

--- app.js
+++ app.js
@@ -6,6 +6,30 @@ require('dotenv').config();
 
 app.use(cors());
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
 app.get('/users', (req, res) => {
   return res.send('GET HTTP method on user resource');
 });
