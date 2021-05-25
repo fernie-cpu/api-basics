ac154c7a37724d6914c960865c227c8588727e1b
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-25 19:44:03 -0300

    Routes to reding the list of users and messages (sample data)

--- app.js
+++ app.js
@@ -21,17 +21,29 @@ let messages = {
   1: {
     id: '1',
     text: 'Hello World',
-    userId: '1',
+    userid: '1',
   },
   2: {
     id: '2',
     text: 'By World',
-    userId: '2',
+    userid: '2',
   },
 };
 
 app.get('/users', (req, res) => {
-  return res.send('GET HTTP method on user resource');
+  return res.send(Object.values(users));
+});
+
+app.get('/users/:userid', (req, res) => {
+  return res.send(users[req.params.userid]);
+});
+
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get('/messages/:messageid', (req, res) => {
+  return res.send(messages[req.params.messageid]);
 });
 
 app.post('/users', (req, res) => {
