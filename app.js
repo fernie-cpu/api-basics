8bf4a067c3d636fa588a7f0a8aaeebbc7f38e608
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-25 19:36:01 -0300

    Routes

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ require('dotenv').config();
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Look at us... again');
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users/:userid', (req, res) => {
+  return res.send(`PUT HTTP method on user/${req.params.userid} resource`);
+});
+
+app.delete('/users/:userid', (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userid} resource`);
 });
 
 app.listen(process.env.PORT, () => {
