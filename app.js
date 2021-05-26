bb7ac658c2fd54993726b20d737b5673bc5c6484
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-26 02:01:59 -0300

    traversy media tutorial

--- app.js
+++ app.js
@@ -1,28 +1,65 @@
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 const app = express();
-require('dotenv').config();
 
-const models = require('./models');
-const routes = require('./routes');
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/session', routes.session);
-app.use('/users', routes.user);
-app.use('/messages', routes.message);
-
-// the user with the identifier 1 which gets assigned as me property to the request object
-app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1],
-  };
-  next();
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'welcome to the api',
+  });
 });
+
+// PROTECTED ROUTE
+app.post('/api/posts', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'post created',
+        authData,
+      });
+    }
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  //mock user
+  const user = {
+    id: 1,
+    username: 'fernie',
+    email: 'fernie@gmail.com',
+  };
+
+  jwt.sign({ user }, 'secretkey', (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
+
+// FORMAT TOKEN
+// Authorization: Bearer <access_token>
+
+//verify token
+function verifyToken(req, res, next) {
+  //Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    // get token from arrar
+    const bearerToken = bearer[1];
+    // set the token
+    req.token = bearerToken;
+    // next middleware
+    next();
+  } else {
+    // forbidden
+    res.sendStatus(403);
+  }
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
