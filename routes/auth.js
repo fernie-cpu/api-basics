abc3aa2b731248ac948d8e74bbe429a683e34182
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-26 08:51:28 -0300

    implement login logic

---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+// POST Login
+router.post('/login', function (req, res, next) {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).json({
+        message: 'Something is not right',
+        user: user,
+      });
+    }
+
+    // set session option to false won't save the user in the session
+    req.login(user, { session: false }, (err) => {
+      if (err) {
+        return res.send(err);
+      }
+
+      // generate a signed son web token with the contents of user object and return it in the response
+      const token = jwt.sign(user, 'jwtSecret');
+      return res.json({ user: token });
+    });
+  })(req, res);
+});
+
+module.exports = router;
