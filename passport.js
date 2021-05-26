bd2b51cccebd16475e47974a6476f91a14f7002c
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-26 09:00:54 -0300

    middleware, allows only requests with valid tokens

--- passport.js
+++ passport.js
@@ -1,5 +1,8 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(
   new LocalStrategy(
@@ -22,3 +25,21 @@ passport.use(
     }
   )
 );
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'jwtSecret',
+    },
+    function (jwtPayload, cb) {
+      //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
+      return UserModel.findOneById(jwtPayload.id)
+        .then((user) => {
+          return cb(null, cb);
+        })
+        .catch((err) => cb(err));
+    }
+  )
+);
+module.exports = passport;
